3bfae75fb89fb5cba47b863cf49c084424cff717
muhammedDev <dimeji.asynctechs@gmail.com>
2025-07-01 15:09:27 +0100

    Update Home component to display title instead of Welcome component

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -9,5 +9,6 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  // return <Welcome />;
+  return <h1 className="text-3xl font-bold text-center mt-4">Colorpop</h1>;
 }
